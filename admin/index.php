4705e3749568499f89770828d2bc40431d1ce62f
Jhon Perez <jhonysperez21@gmail.com>
2025-02-24 23:18:14 -0500

    Filtrado en las tareas

--- admin/index.php
+++ admin/index.php
@@ -4,6 +4,7 @@ include('layout/parte1.php');
 include('../config/controllers/roles/listado_roles.php');
 include('../config/controllers/usuarios/listado_usuarios.php');
 include('../config/controllers/materias/listado_de_materias.php');
+include('../config/controllers/tareas/index.php'); 
 ?>
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
@@ -20,16 +21,13 @@ include('../config/controllers/materias/listado_de_materias.php');
           <!-- small card -->
           <div class="small-box bg-primary">
             <div class="inner">
-
               <?php
               $contador_rol = 0;
               foreach ($roles as $role) {
                 $contador_rol++;
               }
               ?>
-
               <h3><?= $contador_rol; ?></h3>
-
               <p>Roles registrados</p>
             </div>
             <div class="icon">
@@ -45,16 +43,13 @@ include('../config/controllers/materias/listado_de_materias.php');
           <!-- small card -->
           <div class="small-box bg-info">
             <div class="inner">
-
               <?php
               $contador_usuarios = 0;
               foreach ($usuarios as $usuario) {
                 $contador_usuarios++;
               }
               ?>
-
               <h3><?= $contador_usuarios; ?></h3>
-
               <p>Usuarios registrados</p>
             </div>
             <div class="icon">
@@ -72,16 +67,37 @@ include('../config/controllers/materias/listado_de_materias.php');
               <?php
               $contador_materias = 0;
               foreach ($materias as $materia) {
-                $contador_materias = $contador_materias + 1;
+                $contador_materias++;
               }
               ?>
               <h3><?= $contador_materias; ?></h3>
-              <p>Materias registrados</p>
+              <p>Materias registradas</p>
             </div>
             <div class="icon">
               <i class="fas"><i class="bi bi-bookshelf"></i></i></i>
             </div>
             <a href="<?= APP_URL; ?>/admin/materias" class="small-box-footer">
+              Más información <i class="fas fa-arrow-circle-right"></i>
+            </a>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-6">
+          <div class="small-box bg-success">
+            <div class="inner">
+              <?php
+              $contador_tareas = 0;
+              foreach ($tareas as $tarea) {
+                $contador_tareas++;
+              }
+              ?>
+              <h3><?= $contador_tareas; ?></h3>
+              <p>Tareas registradas</p>
+            </div>
+            <div class="icon">
+              <i class="fas"><i class="bi bi-plus-slash-minus"></i></i></i></i>
+            </div>
+            <a href="<?= APP_URL; ?>/admin/tareas" class="small-box-footer">
               Más información <i class="fas fa-arrow-circle-right"></i>
             </a>
           </div>
--- admin/tareas/index.php
+++ admin/tareas/index.php
@@ -10,42 +10,60 @@ include('../../config/controllers/tareas/index.php');
     <div class="content">
         <div class="container">
             <div class="row">
-                <h1>Listado de Tareas</h1>
+                <h1>Listado de Tareas</h1><br>
             </div>
             <div class="row">
                 <div class="col-md-12">
-                    <a href="create.php" class="btn btn-primary">Crear Tarea</a>
-                    <table class="table table-bordered">
-                        <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>Título</th>
-                                <th>Descripción</th>
-                                <th>Fecha de Entrega</th>
-                                <th>Estado</th>
-                                <th>Acciones</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php foreach ($tareas as $tarea): ?>
-                                <tr>
-                                    <td><?= $tarea['id_tarea'] ?></td>
-                                    <td><?= $tarea['titulo'] ?></td>
-                                    <td><?= $tarea['descripcion'] ?></td>
-                                    <td><?= $tarea['fecha_entrega'] ?></td>
-                                    <td><?= $tarea['estado'] ?></td>
-                                    <td>
-                                        <a href="show.php?id=<?= $tarea['id_tarea'] ?>" class="btn btn-info">Ver</a>
-                                        <a href="edit.php?id=<?= $tarea['id_tarea'] ?>" class="btn btn-warning">Editar</a>
-                                        <form action="<?= APP_URL; ?>/config/controllers/tareas/delete.php" method="POST" style="display:inline;">
-                                            <input type="hidden" name="id_tarea" value="<?= $tarea['id_tarea'] ?>">
-                                            <button type="submit" class="btn btn-danger">Eliminar</button>
-                                        </form>
-                                    </td>
-                                </tr>
-                            <?php endforeach; ?>
-                        </tbody>
-                    </table>
+                    <div class="card card-primary shadow-none">
+                        <div class="card-header">
+                            <h3 class="card-title">Tareas registradas</h3>
+                            <div class="card-tools">
+                                <a href="create.php" class="btn btn-dark"> <i class="bi bi-plus-square"></i> Crear nueva tarea </a>
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <table id="example1" class="table table-striped table-bordered">
+                                <thead>
+                                    <tr>
+                                        <th><center>#</center></th>
+                                        <th><center>Título</center></th>
+                                        <th><center>Descripción</center></th>
+                                        <th><center>Fecha de Entrega</center></th>
+                                        <th><center>Estado</center></th>
+                                        <th><center>Acciones</center></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php
+                                    $contador_tarea = 0;
+                                    foreach($tareas as $tarea){
+                                        $contador_tarea++;
+                                        $id_tarea = $tarea['id_tarea'];
+                                    ?>
+                                    <tr>
+                                        <td><?=$contador_tarea;?></td>
+                                        <td><?=$tarea['titulo']?></td>
+                                        <td><?=$tarea['descripcion']?></td>
+                                        <td><?=$tarea['fecha_entrega']?></td>
+                                        <td><?=$tarea['estado']?></td>
+                                        <td style="text-align: center">
+                                            <div class="btn-group" role="group" aria-label="Basic example">
+                                                <a href="show.php?id=<?=$id_tarea;?>" type="button" class="btn btn-info btn-sm"><i class="bi bi-eye"></i></a>
+                                                <a href="edit.php?id=<?=$id_tarea;?>" type="button" class="btn btn-success btn-sm"><i class="bi bi-pencil"></i></a>
+                                                <form action="<?=APP_URL;?>/config/controllers/tareas/delete.php" method="POST">
+                                                    <input type="text" value="<?=$id_tarea;?>" hidden name="id_tarea">
+                                                    <button type="submit" class="btn btn-danger btn-sm"><i class="bi bi-trash"></i></button>
+                                                </form>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    <?php
+                                    }
+                                    ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -55,3 +73,11 @@ include('../../config/controllers/tareas/index.php');
 include ('../../admin/layout/parte2.php');
 include ('../../layout/mostrarMensajes.php');
 ?>
+
+<script>
+  $(function () {
+    $("#example1").DataTable({
+      "responsive": true, "lengthChange": false, "autoWidth": false,
+    }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+  });
+</script>
